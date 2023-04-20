f904577ba707b3719ef9cdb9c304331b28c9fcca
Angela <angelakulola4@gmail.com>
2023-04-20 15:55:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const complementsSet = {};
+
+  for (const num of array) {
+    if (complementsSet[num]) {
+      return true;
+    }
+    complementsSet[target - num] = true;
+  }
+
+  return false;
 }
 
 /* 
@@ -8,10 +18,18 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  1.Create an empty object called complements.
+  2.Iterate through each number num in the input array.
+  3.Check if num is already in complements. If it is, then num is the complement of some previous number, and the function can return true.
+  4.If num is not in complements, add target - num to complements.
+  5.If the function has iterated through the entire array without finding a complement, it returns false.
 */
 
 /*
   Add written explanation of your solution here
+  the function iterates through the array and keeps track of complements seen so far using an object called complementsSet. 
+  If the function encounters a number whose complement (i.e., the difference between the target and the number) has already been seen, it returns true. 
+  If the function reaches the end of the array without finding a pair of numbers that adds up to the target, it returns false.  
 */
 
 // You can run `node index.js` to view these console logs
